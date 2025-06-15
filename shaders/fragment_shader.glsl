25c7f71596acb96deed83423967d9c60567c108b
blink-blade <micah@blinkblade.net>
2025-06-14 23:10:20 -0400

    more shader fun

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -16,8 +16,9 @@ struct PointLight {
     vec3 diffuse;
     vec3 specular;
 };  
-#define NR_POINT_LIGHTS 4  
+#define NR_POINT_LIGHTS 100
 uniform PointLight pointLights[NR_POINT_LIGHTS];
+uniform int pointLightCount;
 
 struct DirLight {
     vec3 direction;
@@ -48,9 +49,9 @@ vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
     vec3 reflectDir = reflect(-lightDir, normal);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     // combine results
-    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
-    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    vec3 ambient  = light.ambient  * vec3(ourColor);
+    vec3 diffuse  = light.diffuse  * diff * vec3(ourColor);
+    vec3 specular = light.specular * spec * vec3(ourColor);
     return (ambient + diffuse + specular);
 }  
 
@@ -67,9 +68,9 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     float attenuation = 1.0 / (light.constant + light.linear * distance + 
   			     light.quadratic * (distance * distance));    
     // combine results
-    vec3 ambient  = light.ambient  * vec3(texture(material.diffuse, TexCoords));
-    vec3 diffuse  = light.diffuse  * diff * vec3(texture(material.diffuse, TexCoords));
-    vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    vec3 ambient  = light.ambient  * vec3(ourColor);
+    vec3 diffuse  = light.diffuse  * diff * vec3(ourColor);
+    vec3 specular = light.specular * spec * vec3(ourColor);
     ambient  *= attenuation;
     diffuse  *= attenuation;
     specular *= attenuation;
@@ -84,7 +85,7 @@ void main() {
     // phase 1: Directional lighting
     vec3 result = CalcDirLight(dirLight, norm, viewDir);
     // phase 2: Point lights
-    for(int i = 0; i < NR_POINT_LIGHTS; i++)
+    for(int i = 0; i < pointLightCount; i++)
         result += CalcPointLight(pointLights[i], norm, FragPos, viewDir);    
     
     FragColor = vec4(result, 1.0);
