268e0bfa6b3416cb24628ccf484106b454b60ef2
blink-blade <micah@blinkblade.net>
2025-07-06 15:59:15 -0400

    Pillar of magic

--- generator.cpp
+++ generator.cpp
@@ -6,6 +6,7 @@
 #include <stddef.h>
 #include "helpers.h"
 #include <vector>
+#include <map>
 
 struct vec2d {
     float x;
@@ -20,9 +21,10 @@ struct corner {
 
 
 
+map<pair<int,int>, struct corner> cornerGrid;
+
 unsigned int seed = 100;
 float **mapNoiseMap;
-struct corner **cornerGrid;
 
 struct vec2d possibleGradientVectors[8];
 
@@ -50,110 +52,53 @@ float interpolate(float a0, float a1, float w) {
 }
  
 // Sample Perlin noise at coordinates x, y
-float noise(float x, float y) {
-
-
-
+float noise(float x, float y, int noiseX, int noiseY) {
+    // cout << "start" << endl;
     // Get the corner positions, x0 is left, x1 is left
-
     int x0 = (int)x; 
-
     int y0 = (int)y;
-
     int x1 = x0 + 1;
-
     int y1 = y0 + 1;
 
-
-
     // Compute Interpolation weights
-
     float sx = x - (float)x0;
-
     float sy = y - (float)y0;
-
-
-
-
-
+    x += noiseX;
+    y += noiseY;
+    cout << x0 << " " << y0 << endl;
+    // cout << sx << ", " << sy << "\n";
+    if (cornerGrid.count(make_pair(x0,y0)) == 0) {
+        // cout << "hey" << endl;
+    }
+    // struct corner tlCorner = cornerGrid[make_pair(x0,y0)];
+    // struct corner blCorner = cornerGrid[make_pair(x0,y1)];
+    // struct corner trCorner = cornerGrid[make_pair(x1,y0)];
+    // struct corner brCorner = cornerGrid[make_pair(x1,y1)];
     struct corner tlCorner;
-
-
     struct corner blCorner;
-
-
     struct corner trCorner;
-
-
     struct corner brCorner;
-
-
-    tlCorner.x = x0, tlCorner.y = y0;
-
-
-    blCorner.x = x0, blCorner.y = y1;
-
-
-    trCorner.x = x1, trCorner.y = y0;
-
-
-    brCorner.x = x1, brCorner.y = y1;
-
-
-    tlCorner.gradientVec = randomGradient(tlCorner.x, tlCorner.y);
-
-
-    blCorner.gradientVec = randomGradient(blCorner.x, blCorner.y);
-
-
-    trCorner.gradientVec = randomGradient(trCorner.x, trCorner.y);
-
-
-    brCorner.gradientVec = randomGradient(brCorner.x, brCorner.y);
-
-
-
+    
     // Get distance vectors(A vector from the corner which points to the tile) for each corner of the octant.
-
     struct vec2d tlDistanceVector;
-
     struct vec2d blDistanceVector;
-
     struct vec2d trDistanceVector;
-
     struct vec2d brDistanceVector;
-
     tlDistanceVector.x = x - tlCorner.x, tlDistanceVector.y = y - tlCorner.y; 
-
     blDistanceVector.x = x - blCorner.x, blDistanceVector.y = y - blCorner.y; 
-
     trDistanceVector.x = x - trCorner.x, trDistanceVector.y = y - trCorner.y; 
-
     brDistanceVector.x = x - brCorner.x, brDistanceVector.y = y - brCorner.y; 
 
-
-
     // Get dot product from the distance vectors and the gradient vectors.
-
     float tlDotProduct = (tlDistanceVector.x * tlCorner.gradientVec.x) + (tlDistanceVector.y * tlCorner.gradientVec.y);
-
     float blDotProduct = (blDistanceVector.x * blCorner.gradientVec.x) + (blDistanceVector.y * blCorner.gradientVec.y);
-
     float trDotProduct = (trDistanceVector.x * trCorner.gradientVec.x) + (trDistanceVector.y * trCorner.gradientVec.y);
-
     float brDotProduct = (brDistanceVector.x * brCorner.gradientVec.x) + (brDistanceVector.y * brCorner.gradientVec.y);
 
-
-
     // Interpolation.
-
     float tlTrInterpolation = interpolate(tlDotProduct, trDotProduct, sx);
-
     float blBrInterpolation = interpolate(blDotProduct, brDotProduct, sx);
-
-
     return interpolate(tlTrInterpolation, blBrInterpolation, sy);
-
 }
 
 int getMaxCornerPos(int noiseWidth, int noiseHeight, int layerAmount, float frequency) {
@@ -168,27 +113,27 @@ int getMaxCornerPos(int noiseWidth, int noiseHeight, int layerAmount, float freq
     return maxPos + 2;
 }
 
-void makeCorners(int noiseWidth, int noiseHeight, int noiseX, int noiseY, int layerAmount, float frequency) {
-    int max = getMaxCornerPos(noiseWidth, noiseHeight, layerAmount, frequency);
-    struct corner corner;
+// void makeCorners(int noiseWidth, int noiseHeight, int noiseX, int noiseY, int layerAmount, float frequency) {
+//     int max = getMaxCornerPos(noiseWidth, noiseHeight, layerAmount, frequency);
+//     struct corner corner;
 
-    delete cornerGrid;
-    // Allocate memory for the array.
-    cornerGrid = new struct corner*[max];
+//     delete cornerGrid;
+//     // Allocate memory for the array.
+//     cornerGrid = new struct corner*[max];
 
-    for (int h = 0; h < max; h++) {
-        cornerGrid[h] = new struct corner[max];
-    }
+//     for (int h = 0; h < max; h++) {
+//         cornerGrid[h] = new struct corner[max];
+//     }
     
-    for (int x = 0; x < max; x++) {
-        for (int y = 0; y < max; y++) {
-            corner.x = x + noiseX / noiseWidth;
-            corner.y = y + noiseY / noiseWidth;
-            corner.gradientVec = randomGradient(x + noiseX / noiseWidth, y + noiseY / noiseWidth);
-            cornerGrid[y][x] = corner;
-        }
-    }
-}
+//     for (int x = 0; x < max; x++) {
+//         for (int y = 0; y < max; y++) {
+//             corner.x = x + noiseX / noiseWidth;
+//             corner.y = y + noiseY / noiseWidth;
+//             corner.gradientVec = randomGradient(x + noiseX / noiseWidth, y + noiseY / noiseWidth);
+//             cornerGrid[y][x] = corner;
+//         }
+//     }
+// }
 
 vector<vector<float>> generateNoiseMap(int noiseWidth, int noiseHeight, int layerAmount, float frequency, float distanceEffect, int islandMode, int noiseX, int noiseY, float distanceCutOffOffsetX, float distanceCutOffOffsetY, float offset, int landOnly, int distanceCutOff, unsigned int newSeed) {
     int x, y, i;
@@ -228,7 +173,7 @@ vector<vector<float>> generateNoiseMap(int noiseWidth, int noiseHeight, int laye
             val = 0;
             freq = frequency;
             for (int i = 0; i < 1; i++) {
-                val += noise((float)x * freq, (float)y * freq) * amp;
+                val += noise((float)x * freq, (float)y * freq, noiseX, noiseY) * amp;
 
                 freq *= 2;
                 amp /= 2;
